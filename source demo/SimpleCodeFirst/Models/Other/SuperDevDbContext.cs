8c7f22a818ac82eef9e8c2dd3f857f925fef3573
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-01-27 14:49:28 +0700

    - Update code

--- source demo/SimpleCodeFirst/Models/Other/SuperDevDbContext.cs
+++ source demo/SimpleCodeFirst/Models/Other/SuperDevDbContext.cs
@@ -5,7 +5,14 @@ namespace Models.Other
     public class SuperDevDbContext:DbContext
     {
         public DbSet<User> Users { get; set; }
+
         public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
         public SuperDevDbContext()
             :base("Data Source=DESKTOP-15KKVN2\\SQLEXPRESS;Initial Catalog=SimpleCodeFirst;Integrated Security=True")
         {
