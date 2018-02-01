890a47c7acc1479dabf670af5ef7f28a02cc9df1
DESKTOP-KDQT218\Thinh <thinhphse62039@gmail.com>
2018-02-01 19:14:53 +0700

    Add solotion

---
+++ students/Thinh Pham/SuperDev.Models/DbContext/SuperDevDbContext.cs
@@ -0,0 +1,43 @@
+﻿using SuperDev.Models.Migrations;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Reflection;
+
+
+namespace SuperDev.Models
+{
+    public partial class SuperDevDbContext : DbContext
+    {
+        public SuperDevDbContext()
+            : base(nameOrConnectionString: "Data Source=DESKTOP-KDQT218\\SQLEXPRESS;Initial Catalog=Sample;Integrated Security=True;User ID=sa;Password=123")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Database.SetInitializer<SuperDevDbContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+
+        public int SaveChanges(int? id = null)
+        {
+            return base.SaveChanges();
+        }
+
+        public void CloneObject(object des, object src)
+        {
+            foreach (PropertyInfo propertyInfo in des.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                if (src.GetType().GetProperty(propertyInfo.Name, BindingFlags.Public | BindingFlags.Instance) != null)
+                {
+                    var value = src.GetType().GetProperty(propertyInfo.Name).GetValue(src, null);
+                    propertyInfo.SetValue(des, value, null);
+                }
+            }
+        }
+    }
+}
