586de7efb7dd6d7acaff88313bf948babeb8725a
Trần Thuận Nghĩa <tran.thuan.nghia@gmail.com>
2018-02-01 16:22:15 +0700

    - Update GHN

---
+++ students/Duy Hòa/GHNAPI/GHN.Service/GHNService.cs
@@ -0,0 +1,41 @@
+﻿using GHN.Service.Models;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace GHN.Service
+{
+    public class GHNService
+    {
+        private HttpClient client;
+
+        public GHNService()
+        {
+            client.BaseAddress = new Uri("https://testapipds.ghn.vn:9999/external/b2c/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<Order> GetOrderAsync(Order order)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "GetOrderInfo", order);
+            if (response.IsSuccessStatusCode)
+            {
+                order = await response.Content.ReadAsAsync<Order>();
+            }
+            return order;
+        }
+        public async Task<ShippingOrder> CreateShippingOrder(ShippingOrder so)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "CreateShippingOrder", so);
+            if (response.IsSuccessStatusCode)
+            {
+                so = await response.Content.ReadAsAsync<ShippingOrder>();
+            }
+            return so;
+        }
+    }
+}
---
+++ students/Duy Hòa/GHNAPI/GHN.Service/Models/ShippingOrder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GHN.Service.Models
+{
+    public class ShippingOrder
+    {
+        public int ClientID { get; set; }
+        public string Password { get; set; }
+        public string ClientOrderCode { get; set; }
+        public string GHNOrderCode { get; set; }
+        public string SealCode { get; set; }
+        public int PickHubID { get; set; }
+        public string FromWardCode { get; set; }
+        public string RecipientName { get; set; }
+        public string RecipientPhone { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string DeliveryDistrictCode { get; set; }
+        public string ToWardCode { get; set; }
+        public double CODAmount { get; set; }
+        public int ServiceID { get; set; }
+        public double Weight { get; set; }
+        public string ContentNote { get; set; }
+        public string ClientNote { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool IsDropOff { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecretKey { get; set; }
+        public double InsuranceFee { get; set; }
+        public long PickupTime { get; set; }
+
+        public string OrderCode { get; set; }
+        public string OrderCodes60P { get; set; }
+        public double TotalFee { get; set; }
+        public string SessionToken { get; set; }
+        public string ErrorMessage { get; set; }
+
+    }
+}
