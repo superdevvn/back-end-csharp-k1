bafecebd47c03cebd2f52a7e9d4c1451bc9d732b
THINHPHSE62039\thinh <thinhphse62039@gmail.com>
2018-02-03 15:32:31 +0700

    SampleAPI

---
+++ students/SampleAPI.Models/Migrations/201802030719182_init001.cs
@@ -0,0 +1,28 @@
+namespace SampleAPI.Models.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init001 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.USers",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FullName = c.String(),
+                        Username = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.USers");
+        }
+    }
+}
---
+++ students/Thinh Pham/SanpleAPI/UserApiController.cs
@@ -0,0 +1,44 @@
+﻿using SampleAPI.Models;
+using SampleAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SanpleAPI
+{
+    public class UserApiController: ApiController
+    {
+        [HttpPost]
+        [Route("api/createUser")]
+       
+        public IHttpActionResult CreateUser(USer user)
+        {
+            var userRepository = new UserRepositories();
+            return Ok(userRepository.Create(user));
+        }
+        [HttpGet]
+        [Route("api/getUsers")]
+        public IHttpActionResult GetUser(USer user)
+        {
+            var userReposity = new UserRepositories();
+            return Ok(userReposity.GetUser());
+        }
+        [HttpGet]
+        [Route("api/getUser/{id}")]
+        public IHttpActionResult GetUserById(Guid id)
+        {
+            var userReposity = new UserRepositories();
+            return Ok(userReposity.GetUserById(id));
+        }
+        [HttpPost]
+        [Route("api/deleteUser")]
+        public IHttpActionResult DeleteUser([FromUri]Guid id)
+        {
+            var userReposity = new UserRepositories();
+            userReposity.Delete(id);
+            return Ok();
+        }
+    }
+}
